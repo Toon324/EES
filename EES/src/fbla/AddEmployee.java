44ec208e6e06508093fe3e80005b9f82c3c4ab6c
Cody Swendrowski <bob324@msn.com>
2013-02-25 17:27:19 -0600

    Pulls data correctly.

--- EES/src/fbla/AddEmployee.java
+++ EES/src/fbla/AddEmployee.java
@@ -4,7 +4,6 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
--- EES/src/fbla/Employees.java
+++ EES/src/fbla/Employees.java
@@ -5,6 +5,8 @@ package fbla;
 
 import java.awt.BorderLayout;
 import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -21,17 +23,21 @@ import javax.swing.JPanel;
  */
 @SuppressWarnings("serial")
 public class Employees extends JPanel implements ActionListener {
-	private Object[] data;
+	private Object[] data = new Object[0];
+	ArrayList<ArrayList<String>> input = new ArrayList<ArrayList<String>>();
 	
 	public Employees() {
-		super(new BorderLayout());
+		super(new GridLayout(3,1));
 		JButton home = new JButton("Home");
 		home.addActionListener(this);
 		JButton addEmployee = new JButton("Add Employee");
 		addEmployee.addActionListener(this);
 		JList employeesList = new JList();
+		employeesList.setVisibleRowCount(10);
+		employeesList.setSize(300, 500);
+		loadDataSource("Resources\\Employees.txt");
 		employeesList.setListData(data);
-		add(employeesList);
+		add("North", employeesList);
 		add(home);
 		add(addEmployee);
 	}
@@ -46,15 +52,17 @@ public class Employees extends JPanel implements ActionListener {
 	}
 	
 	public void loadDataSource(String path) {
-		ArrayList<ArrayList<String>> input = new ArrayList<ArrayList<String>>();
 		Scanner scanner = new Scanner(ListBox.class.getResourceAsStream(path)); //Loads the .txt file
 		scanner.useDelimiter("\t"); //Uses tab as an indicator that a new data segment is present. Can not use comma, as commas may be present in comments.
 		
 		try {
 			while (scanner.hasNextLine()) {
+				String temp = scanner.nextLine();
 				ArrayList<String> line = new ArrayList<String>(); //Gets next line
-				while (scanner.hasNext()) {
-					String s = scanner.next(); //Gets each section of the line
+				Scanner lineScanner = new Scanner(temp);
+				lineScanner.useDelimiter("\t");
+				while (lineScanner.hasNext()) {
+					String s = lineScanner.next(); //Gets each section of the line
 					line.add(s);
 				}
 				input.add(line);
@@ -62,7 +70,20 @@ public class Employees extends JPanel implements ActionListener {
 		} catch (Exception e) {} 
 		finally {
 			scanner.close();
-			data = input.toArray();
+			dataSort(0);
 		}
+	}
+
+	private void dataSort(int i) {
+		
+		data = new Object[input.size()];
+		for (int x=0; x<input.size(); x++) {
+			StringBuilder line = new StringBuilder();
+			for (String s: input.get(x))
+				line.append(s + " ");
+			data[x] = line;
+		}
+		//data = input.toArray();
+		
 	}
 }
