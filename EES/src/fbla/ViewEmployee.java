4c216bfc799d17d3c30d0b6375d1374cfe2566ff
Cody Swendrowski <cswendrowski@mail.com>
2013-04-30 21:06:00 -0500

    Fixes error where data in ViewEmployee didn't match correctly.

--- EES/src/fbla/ViewEmployee.java
+++ EES/src/fbla/ViewEmployee.java
@@ -26,27 +26,28 @@ public class ViewEmployee extends DataWindow {
 	 */
 	public ViewEmployee(int num) {
 		super(new BorderLayout());
-		
+
 		employeeNum = num;
-		
+
 		JButton close = new JButton("Close");
 		JButton viewEvals = new JButton("View Evaluations");
-		
+
 		close.addActionListener(this);
 		viewEvals.addActionListener(this);
-		
-		JPanel buttons = new JPanel(new GridLayout(0,1));
+
+		JPanel buttons = new JPanel(new GridLayout(0, 1));
 		JPanel info = new JPanel(new GridLayout(0, 1));
 		JPanel categories = new JPanel(new GridLayout(0, 1));
 
 		buttons.add(viewEvals);
 		buttons.add(close);
-		
+
 		// The method frame.pack() sets elements based on max width of contained
 		// items. This string is larger to give a padding between data points.
 		categories.add(new JLabel("Average Evaluation Score     "));
 		categories.add(new JLabel("First Name"));
 		categories.add(new JLabel("Last Name"));
+		categories.add(new JLabel("Email"));
 		categories.add(new JLabel("Phone Num"));
 		categories.add(new JLabel("Cell Num"));
 		categories.add(new JLabel("Address"));
@@ -57,16 +58,44 @@ public class ViewEmployee extends DataWindow {
 		// Loads in employee data
 		try {
 			File file = new File(EES.employeesPath);
+			File evals = new File(EES.evalPath);
 			Scanner scanner = new Scanner(file);
+			Scanner evalScanner = new Scanner(evals);
 
 			while (scanner.hasNextLine()) {
 				String line = scanner.nextLine();
 				Scanner lineScanner = new Scanner(line);
 				lineScanner.useDelimiter(EES.delim);
-				if (lineScanner.nextInt() == employeeNum)
+				int foundNum = lineScanner.nextInt();
+				if (foundNum == employeeNum) {
+					String evalAvg = "No Evaluations";
+					while (evalScanner.hasNextLine()) {
+						String tempLine = evalScanner.nextLine();
+						
+						Scanner avglineScanner = new Scanner(tempLine);
+						avglineScanner.useDelimiter(EES.delim);
+						
+						avglineScanner.next(); //Discard evaluation number
+						
+						int numFound = Integer.parseInt(avglineScanner.next().replace("﻿", "")); //Employee number
+						
+						for (int x=0; x<11; x++)
+							avglineScanner.next(); //Discard other eval info
+						
+						if (numFound == employeeNum) {
+							evalAvg = avglineScanner.next();
+							break;
+						}
+						avglineScanner.close();
+					}
+					
+					info.add(new JLabel(evalAvg));
+					evalScanner.close();
+					
 					while (lineScanner.hasNext())
 						info.add(new JLabel(lineScanner.next()));
 
+				}
 				lineScanner.close();
 			}
 
@@ -78,7 +107,7 @@ public class ViewEmployee extends DataWindow {
 		add(BorderLayout.WEST, categories);
 		add(info);
 		add(BorderLayout.PAGE_END, buttons);
-		
+
 		frame = EES.createAndShowGUI(this);
 		frame.setTitle("Displaying Employee "
 				+ EES.getEmployeeName(employeeNum));
